7b2e6fbee1e9c25162cc03c8e6b6d6a7bb4af892
Lucas <lucasgunner3@gmail.com>
2021-12-21 20:14:57 -0300

    Adicionar arquivos de projeto.

---
+++ src/Solid.Srp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Solid.Srp;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+    }
+
+    static void CreateClient()
+    {
+    }
+
+    static void ReadClient()
+    {
+    }
+
+    static void DeleteClient()
+    {
+    }
+
+    static void NotifyClient()
+    {
+    }
+}
